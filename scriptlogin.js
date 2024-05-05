d0164ae3c8dbbca7185b7d728ab54ad7a4417c76
alfianda <80555881+alfianda@users.noreply.github.com>
2024-05-05 17:12:12 +0700

    Update scriptlogin.js
    Perbaikan. Href

--- scriptlogin.js
+++ scriptlogin.js
@@ -5,12 +5,10 @@ function login() {
     // Check if username and password are default "admin"
     if (username === "admin" && password === "admin") {
         window.location.href = "home.html"; // Redirect to main page if username and password are correct
-    if (username === "shelina" && password === "shelina") {
-        window.location.href = "home.html";
     } else if (username === "newuser" && password === "newpassword") {
         // Add new username and password combination here
         // For example, let's use "newuser" as username and "newpassword" as password
-        window.location.href = "home.html"; // Redirect to main page if username and password are correct
+        window.location.href = "---.html"; // Redirect to main page if username and password are correct
     } else {
         document.getElementById("error-message").innerText = "Username atau password Anda salah"; // Display error message
     }
