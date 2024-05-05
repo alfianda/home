e55b6e11144c29e5936aacca1986299f572ed5f7
alfianda <80555881+alfianda@users.noreply.github.com>
2024-05-05 15:40:06 +0700

    Update scriptlogin.js

--- scriptlogin.js
+++ scriptlogin.js
@@ -4,7 +4,7 @@ function login() {
 
     // Check if username and password are default "admin"
     if (username === "admin" && password === "admin") {
-        window.location.href = "main.html"; // Redirect to main page if username and password are correct
+        window.location.href = "home.html"; // Redirect to main page if username and password are correct
     } else if (username === "newuser" && password === "newpassword") {
         // Add new username and password combination here
         // For example, let's use "newuser" as username and "newpassword" as password
